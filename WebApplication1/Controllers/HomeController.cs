1d759c5a5dd2efe203c0b0ddb6cdb51ad658f836
Leo <e23882@hotmail.com>
2020-02-18 18:49:12 +0800

    first version commit

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public class Priority
+        {
+            public string Name { get; set; }
+            public int Level { get; set; }
+            public Priority(){}
+
+            public Priority(string name, int lv)
+            {
+                this.Name = name;
+                this.Level = lv;
+            }
+        }
+        public ActionResult Index()
+        {
+            List<Priority>  list= new List<Priority>();
+            list.Add(new Priority("Leo", 1));
+            list.Add(new Priority("Chris", 2));
+
+            ViewBag.List = list;
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Test(string key)
+        {
+            if (!string.IsNullOrEmpty(key))
+            {
+                Priority pp = new Priority();
+                pp.Name = "admin";
+                pp.Level = 999;
+                ViewBag.Priority = pp;
+                return View();
+            }
+            else
+            {
+                return View();
+            }
+
+        }
+    }
+}
